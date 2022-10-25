70a1edf333e2adabe5f87834f86c9902554876ca
jakehardin <jakehardin13@gmail.com>
2022-10-24 19:29:52 -0500

    about to do some damage

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
 const pets = [
 {
   id: 30,
-name: "Dave",
-color: "Red",
+name: "Harry Potter",
+color: "Griffyndor",
 specialSkill: "Use to think conspiracy theories were fun.",
-type: "dino",
-imageUrl: "https://www.lintelligencer.com/wp-content/uploads/2020/07/Study-Famous-Jurassic-Park-dinosaur-the-Dilophosaurus-was-bigger-and-more-powerful-than-the-1993-film-shows.jpg"
+type: "Griffyndor",
+imageUrl: "https://pndblog.typepad.com/.a/6a00e0099631d0883301b7c905fa29970b-800wi"
+},
+
+{
+  id: 30,
+name: "Hermoine Granger",
+color: "Griffyndor",
+specialSkill: "Use to think conspiracy theories were fun.",
+type: "Griffyndor",
+imageUrl: "http://images4.fanpop.com/image/photos/20000000/Hermione-Granger-hermione-granger-20053436-936-1197.jpg"
+},
+
+{
+  id: 30,
+name: "Ron Weasley",
+color: "Griffyndor",
+specialSkill: "Use to think conspiracy theories were fun.",
+type: "Griffyndor",
+imageUrl: "https://pm1.narvii.com/6912/a1ec19d6f11bf0557674c1d84c4c81510b640e9br1-315-399v2_hq.jpg"
+},
+
+{
+  id: 30,
+name: "Draco Malfoy",
+color: "Griffyndor",
+specialSkill: "Use to think conspiracy theories were fun.",
+type: "Slytherin",
+imageUrl: "http://media3.popsugar-assets.com/files/2014/08/12/890/n/1922283/60159972105ee0e2_-Harry-Potter-the-Philosophers-Stone-dramione-bRmhEa.xxxlarge_2x/i/Draco-Malfoy-played-Tom-Felton.jpg"
+},
+
+{
+  id: 30,
+name: "Jesse Pinkman",
+color: "Griffyndor",
+specialSkill: "Use to think conspiracy theories were fun.",
+type: "Hufflepuff",
+imageUrl: "https://vignette.wikia.nocookie.net/wfl/images/5/55/Jesse_Pinkman.jpg/revision/latest?cb=20190308081818"
+},
+
+{
+  id: 30,
+name: "Walter White",
+color: "Griffyndor",
+specialSkill: "Use to think conspiracy theories were fun.",
+type: "Ravenclaw",
+imageUrl: "https://yt3.ggpht.com/a/AATXAJzaZnwwtOHktX2TAdmzGxKd7HP9GbiPcUWZ7A=s900-c-k-c0xffffffff-no-rj-mo"
 }
 ];
 
@@ -23,13 +68,13 @@ for (const animal of array) {
 domString += `<div class="card mb-3" style="max-width: 540px;">
 <div class="row g-0">
   <div class="col-md-4">
-    <img src="..." class="img-fluid rounded-start" alt="...">
+    <img src="${animal.imageUrl}" class="img-fluid rounded-start" alt="...">
   </div>
   <div class="col-md-8">
     <div class="card-body">
     <div class="card-header">${animal.name}</div>
-      <p class="card-text">${animal.color}</p>
-      <button class="btn btn-danger" id="delete--${animal.id}">Delete</button>
+      <p class="card-text">${animal.type}</p>
+      <button class="btn btn-danger" id="delete--${animal.id}">Expel</button>
     </div>
   </div>
 </div>
@@ -54,7 +99,9 @@ return petTypeArray;
 
 const allPetsButton = document.querySelector("#all-button");
 
-const dogsButton = document.querySelector("#dog-button");
+const dogsButton = document.querySelector("#grif-button");
+
+const slythButton = document.querySelector("#slyth-button");
 
 const catsButton = document.querySelector("#cat-button");
 
@@ -65,17 +112,22 @@ petsOnDom(pets);
 });
 
 dogsButton.addEventListener('click', () => {
-const petsAreDogs = filter(pets, 'dog');
+const petsAreDogs = filter(pets, 'Griffyndor');
 petsOnDom(petsAreDogs);
 });
 
+slythButton.addEventListener('click', () => {
+const petsAreSlyth = filter(pets, 'Slytherin');
+petsOnDom(petsAreSlyth);
+});
+
 catsButton.addEventListener('click', () => {
-const petsAreCats = filter(pets, 'cat');
+const petsAreCats = filter(pets, 'Hufflepuff');
 petsOnDom(petsAreCats);
 });
 
 dinosButton.addEventListener('click', () => {
-const petsAreDinos = filter(pets, 'dino');
+const petsAreDinos = filter(pets, 'Ravenclaw');
 petsOnDom(petsAreDinos);
 });
 
@@ -86,7 +138,7 @@ e.preventDefault(); // EVERY TIME YOU CREATE A FORM
 
 const newPetObj = {
 id: pets.length + 1,
-name: document.querySelector("#name").value,
+name: document.querySelector("#name").value
 }
 
 pets.push(newPetObj);
